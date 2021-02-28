c5be3c1cd315f0bde2af78d3147f229a04d88b6a
Adeyemi Adekanye <sundayadeyemi8@gmail.com>
2021-02-28 23:33:41 +0100

    Hash String Assignment

---
+++ Hashstring_assign/hert.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#creating a hash function
+sha256sum Backend_Monday_wk6.jpeg>>file.txt
+sha256sum Backend_Tuesday_wk6.jpeg>>file.txt
+sha256sum Backend_Wednesday_wk6.jpeg>>file.txt
+sha256sum Backend_Thursday_wk6.jpeg>>file.txt
+sha256sum Backend_friday_wk6.jpeg>>file.txt
+exit 0
+
