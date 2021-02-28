11991896307a77c4842bd9fb6b63975f9ec94d31
adedolapo <shayne.doly94@gmail.com>
2021-02-28 22:42:38 +0100

    sha codes for week's excalidraw

---
+++ excalidraw.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sha1sum monday.jpeg >> hash_code.txt
+
+sha1sum tuesday.jpeg >> hash_code.txt
+
+sha1sum wednesday.jpeg >> hash_code.txt
+
+sha1sum thursday.jpeg >> hash_code.txt
+
+sha1sum friday.jpeg >> hash_code.txt
